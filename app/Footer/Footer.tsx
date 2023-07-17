b39658cf82b34ddc1dfeda686a9af8ff1ed85dd2
Yoga <im.brindawan@gmail.com>
2023-07-17 17:34:07 +0800

    Responsive Footer BETA

--- app/Footer/Footer.tsx
+++ app/Footer/Footer.tsx
@@ -6,16 +6,16 @@ export default function Footer() {
                 <img src='/img/LOGO UDAYANA.svg' alt="Logo" />
                 <p className="w-80 left-[51px] top-[202px] text-slate-500 text-lg font-medium leading-7">Jln P.B. Sudirman, Denpasar <br />Email: info@unud.ac.id <br />Tlp : +62 (361) 223797 <br />Fax : +62 (361) 701907</p>
             </div>
-            
+
             {/* Footer Link */}
-            <div>
-                <div>
+            <div className="flex md:flex-row flex-col " >
+                <div className="">
                     <div className="w-40 left-0 top-0 text-slate-800 text-xl font-bold leading-7">Prabhu Udayana</div>
                     <div className="left-0 top-[44px] text-slate-500 text-base font-medium leading-normal">Filosofi Logo & Media</div>
                     <div className="left-0 top-[84px] text-slate-500 text-base font-medium leading-normal">Merchandise</div>
                     <div className="left-0 top-[124px] text-slate-500 text-base font-medium leading-normal">Sponsorship</div>
                 </div>
-                <div>
+                <div className="">
                     <div className="w-52 left-0 top-0 text-slate-800 text-xl font-bold leading-7">Universitas Udayana</div>
                     <div className="left-0 top-[44px] text-slate-500 text-base font-medium leading-normal">Website Resmi</div>
                     <div className="left-0 top-[84px] text-slate-500 text-base font-medium leading-normal">Fakultas</div>
@@ -26,25 +26,7 @@ export default function Footer() {
             </div>
 
             {/* Footer Copyright */}
-            <div className="left-[610px] top-[409px] text-slate-400 text-sm font-medium leading-tight">© 2023 USDI. All rights reserved.</div>
-
-            {/* <div className="w-80 left-[51px] top-[202px] absolute text-slate-500 text-lg font-medium leading-7">Jln P.B. Sudirman, Denpasar Email: info@unud.ac.id  Tlp : +62 (361) 223797   Fax : +62 (361) 701907</div>
-            <div className="w-44 h-36 left-[995px] top-[88px] absolute">
-                <div className="w-40 left-0 top-0 absolute text-slate-800 text-xl font-bold leading-7">Prabhu Udayana</div>
-                <div className="left-0 top-[44px] absolute text-slate-500&gt;text-base font-medium leading-normal">Filosofi Logo & Media</div>
-                <div className="left-0 top-[84px] absolute text-slate-500 text-base font-medium leading-normal">Merchandise</div>
-                <div className="left-0 top-[124px] absolute text-slate-500 text-base font-medium leading-normal">Sponsorship</div>
-            </div>
-            <div className="w-52 h-56 left-[1200px] top-[88px] absolute">
-                <div className="w-52 left-0 top-0 absolute text-slate-800 text-xl font-bold leading-7">Universitas Udayana</div>
-                <div className="left-0 top-[44px] absolute text-slate-500 text-base font-medium leading-normal">Website Resmi</div>
-                <div className="left-0 top-[84px] absolute text-slate-500 text-base font-medium leading-normal">Fakultas</div>
-                <div className="left-0 top-[124px] absolute text-slate-500 text-base font-medium leading-normal">UKM</div>
-                <div className="left-0 top-[164px] absolute text-slate-500 text-base font-medium leading-normal">Informasi</div>
-                <div className="left-0 top-[204px] absolute text-slate-500 text-base font-medium leading-normal">Support</div>
-            </div>
-            <div className="w-96 h-px left-0 top-[369px] absolute border border-gray-100"></div>
-            <div className="left-[610px] top-[409px] absolute text-slate-400 text-sm font-medium leading-tight">© 2023 USDI. All rights reserved.</div> */}
+            {/* <div className="left-[610px] top-[409px] text-slate-400 text-sm font-medium leading-tight">© 2023 USDI. All rights reserved.</div>   */}
 
         </footer>
     )
